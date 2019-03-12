7f44fd76c3eac1e77f85198727660ad304c389af
alreadyExisted <vit.polyanskiy@gmail.com>
2019-03-12 23:35:06 +0200

    Added creating path part

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,17 @@ import { Chart } from './chart.js'
 
 fetchAsync('/data/chart_data.json').then(data => {
   const wrapper = document.getElementById('js-charts')
-  data.forEach((chartData, index) => {
+  data.forEach(chartData => {
     const element = document.createElement('div')
     element.className = 'chart'
     wrapper.appendChild(element)
     const chart = new Chart({
-      element
+      element,
+      opts: {
+        data: [
+          ...chartData.columns
+        ]
+      }
     })
     
     chart.draw()
--- scripts/chart.js
+++ scripts/chart.js
@@ -2,15 +2,17 @@ import { createElement } from './utils.js'
 
 export class Chart {
   constructor({ element, opts = {} }) {
-    this.init({ element })
+    this.init({ element, opts })
   }
 
-  init({ element }) {
+  init({ element, opts }) {
     if (typeof element === 'string') {
       this._element = document.querySelector(element)
     } else {
       this._element = element
     }
+
+    this._data = opts.data
     
     this._svg = createElement('svg')
     this._svg.setAttribute('preserveAspectRatio', 'xMinYMin slice')
@@ -29,6 +31,7 @@ export class Chart {
     
 
     const path = createElement('path')
+    path.setAttribute('fill', 'none')
     path.setAttribute(
       'stroke',
       `rgb(
@@ -43,7 +46,23 @@ export class Chart {
   }
 
   createPath() {
-    return `M${this.width * .2},${this.height * .8} ${this.width * .8},${this.height * .2}`
+    const x = this._data[0].slice(1).map((_, i) => i)
+    const y1 = this._data[1].slice(1)
+    const y2 = this._data[2].slice(1)
+    const yMax = Math.max(Math.max(...y1), Math.max(...y2))
+    const xPerc = this.width / x.length
+    const yPerc = this.height / yMax
+
+    let d = `M${xPerc * x[0]},${yPerc * y1[0]}`
+
+    x.forEach(item => {
+      const index = item + 1
+      if (index <= x.length - 1) {
+        d += `L${xPerc * index},${yPerc * y1[index]}`
+      }
+    })
+
+    return d
   }
 
   get width() {
